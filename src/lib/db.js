419388712977216892324fb39758987afdbe4d21
Sachin <sachinkaintura2002@gmail.com>
2025-09-04 23:50:38 +0530

    added things

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,9 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: 'localhost',
-  port: 3308, // change to 3306 if using default MySQL port
-  user: 'root', // change if you use a different user
-  password: 'KKrr@123', // replace with your MySQL password
-  database: 'school_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
 });
