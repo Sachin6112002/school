4f175c241ee160676988110106c6e8e4be4ba44e
Sachin <sachinkaintura2002@gmail.com>
2025-09-04 23:54:04 +0530

    some error during delopement

--- src/app/schools/page.jsx
+++ src/app/schools/page.jsx
@@ -11,13 +11,12 @@ export default function SchoolsPage() {
     <div className="max-w-2xl mx-auto p-4">
       <div className="flex gap-4 mb-6 justify-center">
         <Link href="/addSchool"
-          className={`px-4 py-2 rounded font-semibold ${tab === "add" ? "bg-blue-600 text-white" : "bg-gray-200"}`}
+          className="px-4 py-2 rounded font-semibold  bg-blue-600 text-white "
           
         >
           Add School
         </Link>
-        <Link href="/showSchools"
-          className={`px-4 py-2 rounded font-semibold ${tab === "show" ? "bg-blue-600 text-white" : "bg-gray-200"}`}
+        <Link href="/showSchools" className="px-4 py-2 rounded font-semibold  bg-blue-600 text-white " 
          
         >
           Show Schools
